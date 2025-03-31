f137fa797dabb504886d8c595e2af82bc0b91a5d
Xu Yiming <xuyiming.open@outlook.com>
2025-04-01 00:32:02 +0800

    feat: Add PowerShell impl

---
+++ Outlook.ps1
@@ -0,0 +1,29 @@
+# Check if running as administrator
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    Try {
+        Start-Process (Get-Process -PID $PID).Path -ArgumentList "-File", $PSCommandPath -Verb RunAs
+        Exit
+    }
+    Catch {
+		Write-Error -Message "Failed to run as Administrator. Please rerun with elevated privileges."
+        Exit
+    }
+}
+
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1 -Force > $null
+Write-Information  -Message "Uninstalling the original version (reffer to readme for errors/red text)"
+Get-AppxPackage -AllUsers Microsoft.OutlookForWindows | Remove-AppxPackage -AllUsers
+Write-Information  -Message "installing the patched one (Errors are bad now)"
+switch ($Env:PROCESSOR_ARCHITECTURE) {
+	"AMD64" {
+		Add-AppxPackage -register "$PSScriptRoot\AppxManifest.xml"
+	}
+	"x86" {
+		Add-AppxPackage -register "$PSScriptRoot\AppxManifestx86.xml"
+	}
+	"ARM64" {
+		Add-AppxPackage -register "$PSScriptRoot\AppxManifest-ARM64.xml"
+	}
+}
+Write-Information  -Message "Done!"
+Read-Host -Prompt "Press any key to close this window"
